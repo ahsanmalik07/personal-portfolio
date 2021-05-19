ec9569740b8b8702c11daa261320f42bf74fa2f3
ahsanmalik07 <ahsann.malik7@gmail.com>
2021-05-19 12:57:49 +0500

    updated

---
+++ sandbox.js
@@ -0,0 +1,15 @@
+function seeMorePrjs() {
+
+    document.getElementById("prj-1").classList.remove('d-none')
+
+}
+
+
+
+function hide() {
+    document.getElementById("hidden-project-one").classList.add('d-none')
+    document.getElementById("hidden-project-two").classList.add('d-none')
+    document.getElementById("hidden-project-three").classList.add('d-none')
+    document.getElementById("hide-btn").classList.add('d-none')
+    document.getElementById("see-more-btn").classList.remove('d-none')
+}
